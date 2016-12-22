24ce30e94de27543201f75494d2b87388af3857a
Barış Arıburnu <barisariburnu@gmail.com>
2016-12-22 06:13:47 +0000

    Update parser method [benimlegez]

--- viaspider/spiders/benimlegez.py
+++ viaspider/spiders/benimlegez.py
@@ -2,8 +2,7 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 from scrapy.http import Request
-from viaspider.items import ViaspiderItem
-from viaspider.settings import SUMMARY_LIMIT
+from viaspider.items import ItemParser
 
 
 class BenimlegezSpider(CrawlSpider):
@@ -32,37 +31,5 @@ class BenimlegezSpider(CrawlSpider):
             yield Request(url.extract(), callback=self.parse_post)
 
     def parse_post(self, response):
-        item = ViaspiderItem()
-        item['url'] = response.url
-        item['source'] = 'benimlegez.com'
-        item['title'] = self.get_title(response)
-        item['summary'] = self.get_summary(response)
-        item['categories'] = self.get_categories(response)
-        item['tags'] = self.get_tags(response, item['categories'])
-        item['image'] = self.get_image(response)
-        item['created'] = self.get_created(response)
-        return item
-        
-    def get_title(self, response):
-        title = response.xpath('//head/title/text()').extract()[0].encode('utf-8')
-        return ("-".join(title.split("-")[:-1])).strip() if title.endswith(' - BenimleGez') else title
-        
-    def get_summary(self, response):
-        summary = response.xpath('//head/meta[@property="og:description"]/@content').extract()[0]
-        return summary[:-SUMMARY_LIMIT] if len(summary) > SUMMARY_LIMIT else summary
-        
-    def get_categories(self, response):
-        categories = response.xpath('//head/meta[@property="article:section"]/@content').extract()
-        return categories
-        
-    def get_tags(self, response, categories):
-        tags = response.xpath('//head/meta[@property="article:tag"]/@content').extract()
-        return tags if len(tags) > 0 else categories
-        
-    def get_image(self, response):
-        image = response.xpath('//head/meta[@property="og:image"]/@content').extract()[0]
-        return image
-        
-    def get_created(self, response):
-        created = response.xpath('//head/meta[@property="article:published_time"]/@content').extract()[0]
-        return created
+        item = ItemParser('benimlegez.com', response, '-', 'BenimleGez')
+        return item.parse()
